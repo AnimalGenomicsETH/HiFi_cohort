06c485bb6f4f814a7540ba9f8ea460d3f44afc09
ASLeonard <tkom343@gmail.com>
2024-06-12 16:11:33 +0200

    generalise DUP genotyping

--- scripts/manual_DUP_validation.sh
+++ scripts/manual_DUP_validation.sh
@@ -1,1 +1,15 @@
-{ echo -e "sample\tbefore\tin\tafter" ; for i in *cram; do echo -ne "${i%.bwa.cram}\t"; samtools bedcov --reference /cluster/work/pausch/inputs/ref/BTA/UCD2.0/GCA_002263795.4_ARS-UCD2.0_genomic.fa  <(echo -e "8\t102486033\t103486033\n8\t103486033\t103571583\n8\t103571583\t104571583") $i | awk '{print $4/($3-$2)}' |tr '\n' '\t'; echo ; done ; }
+
+C=$1
+S=$2
+E=$3
+W=$4
+
+A=$((S-W))
+R=$((E+W))
+
+echo -e "sample\tbefore\tin\tafter"
+for i in /nfs/nas12.ethz.ch/fs1201/green_groups_tg_public/data/BTA/bams_UCD2.0_eQTL_SR/*cram
+do
+  echo -ne "$(basename ${i%.bwa.cram})\t"; samtools bedcov --reference /cluster/work/pausch/inputs/ref/BTA/UCD2.0/GCA_002263795.4_ARS-UCD2.0_genomic.fa  <(echo -e "$C\t$A\t$S\n$C\t$S\t$E\n$C\t$E\t$R") $i | awk '{print $4/($3-$2)}' |tr '\n' '\t' 
+  echo
+done
