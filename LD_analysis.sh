8963887be1ef8e83b12ed527d34aa81ec4d34bc2
ASLeonard <tkom343@gmail.com>
2024-12-16 15:22:40 +0100

    get all r2 down to 0

--- LD_analysis.sh
+++ LD_analysis.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 for i in {1..29}
 do
   bcftools annotate --set-id "%VKX" -o ${i}.hex.vcf.gz -W=tbi filtered.${i}.vcf.gz
   zgrep "SVTYPE=" ${i}.hex.vcf.gz | cut -f 3 > ${i}.SV_list
-  plink2 --vcf ${i}.hex.vcf.gz --out SVs_${i} --threads 2  --vcf-half-call missing --ld-snp-list ${i}.SV_list --r2-unphased zs --ld-window 999999999 --cow
+  plink2 --vcf ${i}.hex.vcf.gz --out SVs_${i} --threads 2  --vcf-half-call missing --ld-snp-list ${i}.SV_list --r2-unphased zs --ld-window-r2 0 --ld-window 999999999 --cow
 done
